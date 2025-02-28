6f9b99efa3a9601c85e1956442ea39ab459a2633
Ashraf Khan <ashrafiim071@gmail.com>
2025-02-28 23:19:07 +0500

    initial commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Tilt from "react-tilt";
 import { motion } from "framer-motion";
+import Tilt from "react-parallax-tilt"; // Updated import
 
 import { styles } from "../styles";
 import { services } from "../constants";
@@ -8,25 +8,23 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className="xs:w-[250px] w-full mx-auto">
+  <Tilt
+    tiltMaxAngleX={45} // Replaces max prop from react-tilt
+    tiltMaxAngleY={45} // Replaces max prop from react-tilt
+    scale={1} // scale prop remains the same
+    speed={450} // speed prop remains the same
+    className="xs:w-[250px] w-full mx-auto"
+  >
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
     >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
-      >
+      <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
         <img
           src={icon}
           alt="web-development"
           className="w-16 h-16 object-contain"
         />
-
         <h3 className="text-white text-[20px] font-bold text-center">
           {title}
         </h3>
